fa974df5e1eb380e1efbce6fc21c8d20e263344f
JHONSON <114837630+Jhonsonwannaa@users.noreply.github.com>
2024-05-29 02:57:13 +0100

    Update dorker.py

--- dorker.py
+++ dorker.py
@@ -1,6 +1,70 @@
-import base64
+#!/bin/python3
+
+from googlesearch import search
+from colorama import Fore, Style
+import argparse
 
 
-eval(compile(base64.b64decode('ZnJvbSBnb29nbGVzZWFyY2ggaW1wb3J0IHNlYXJjaAppbXBvcnQgc3lzCgpmcm9tIHB5ZmlnbGV0IGltcG9ydCBGaWdsZXQKCmNsYXNzIGNvdWxldXI6CgkJICAJT0sgPSAnXDAzM1s5MW0nICNHUkVFTgoJCSAgCQoJCSAgCQkKCmZpZ2xldCA9IEZpZ2xldChmb250PSdzbGFudCcpCnJlc3VsdCA9IGZpZ2xldC5yZW5kZXJUZXh0KCJZcyBqaG9uc29uIikKZGFrPSBmaWdsZXQucmVuZGVyVGV4dCgiTGUgd2FuYSIpCnByaW50KGNvdWxldXIuT0srcmVzdWx0KQpwcmludChjb3VsZXVyLk9LK2RhaykKdHJ5IDoKCWEgPSBzeXMuYXJndlsxXQoJcXVlcnkgPSBhCglmb3IgZG9yayBpbiBzZWFyY2gocXVlcnksIHRsZD0iY28uaW4iLCBudW09MTAwLCBzdG9wPTEwMCwgcGF1c2U9Mik6CgkJICAJCWNsYXNzIGNvdWxldXI6CgkJICAJCQlPSyA9ICdcMDMzWzkybScgI0dSRUVOCgkJICAJCXByaW50KGNvdWxldXIuT0srZG9yaykKCgpleGNlcHQgOgoJY2xhc3MgY291bGV1cjoKCQlPSyA9ICdcMDMzWzkybScgI0dSRUVOCgkKCXByaW50KGNvdWxldXIuT0srJ3B5dGhvbiBkb3JrZXIucHkgdGFyZ2V0Jyk='),'<string>','exec'))
+
+def arparse():
+    banner='''
+    ____             __   _______           __
+   / __ \____  _____/ /__/ ____(_)___  ____/ /
+  / / / / __ \/ ___/ //_/ /_  / / __ \/ __  / 
+ / /_/ / /_/ / /  / ,< / __/ / / / / / /_/ /  
+/_____/\____/_/  /_/|_/_/   /_/_/ /_/\__,_/   
+                                 v.0.1            
+                                 Author :Jhonson
+                                 Email : wannaajhonson@gmail.com
+    '''
+
+    print(Fore.GREEN + Style.BRIGHT +banner+ Style.RESET_ALL)
+
+arparse()
+
+def main(title,num,save): 
+   
+ try :  
+   launch=input(str(Fore.RED +Style.BRIGHT +'Filter results YES OR NO : '+ Style.RESET_ALL)).upper()
+   print('\n')
+
+   if launch == 'YES':
+        filter = search(title, num_results=int(num), lang="en")
+        for i in filter:
+            if args.title in i:
+                print(Fore.BLUE +Style.BRIGHT +i+ Style.RESET_ALL)
+                if args.save:
+                   with open(save, 'a') as file :
+                     
+                      file.write(i+'\n')
+        objet=f'File save {args.save}'
+        print(Fore.YELLOW +Style.BRIGHT +f'{objet}'+ Style.RESET_ALL)
+                  
+   else : 
+        filter = search(title, num_results=int(args.num), lang="en")
+        for i in filter:
+           
+            print(Fore.BLUE +Style.BRIGHT +i+ Style.RESET_ALL)
+            if args.save:
+               with open(args.save, 'a') as file :
+                   file.write(i+'\n') 
+ 
+   
+   if  launch != 'YES':
+        objet=f'File save {args.save}'        
+        print(Fore.YELLOW +Style.BRIGHT +f'{objet}'+ Style.RESET_ALL)
+
+ except requests.exceptions.ReadTimeout:
+
+   print(Fore.GREEN +Style.BRIGHT +'Resend Problem Connexion'+ Style.RESET_ALL)
 
 
+  
+if __name__=='__main__':
+
+       parser = argparse.ArgumentParser(description="DorkFind")
+       parser.add_argument("-title", "--title", dest="title", help="title of your dork", required=True)
+       parser.add_argument("-num", "--num", dest="num", help="nomber of results", required=True)     
+       parser.add_argument("-save", "--save", dest="save", help="save", required=False)  
+       args = parser.parse_args()
+       main(args.title, args.num, args.save)
